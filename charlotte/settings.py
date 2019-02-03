802c4092145f09a5011a72c69eca5ace3b7f6744
Jack <babybishops@gmail.com>
2019-02-03 19:34:02 +0100

    Use YAML for config file

--- charlotte/settings.py
+++ charlotte/settings.py
@@ -1,13 +1,14 @@
 import pathlib
 import shutil
 
-from configparser import ConfigParser
+import yaml
+
 from charlotte import app
 
 def get_blog_name():
     try:
-        config_file.read(CONFIG_PATH)
-        return config_file.get("blog", "Blog Name")
+        config = get_config()
+        return config["blog"]["name"]
     except:
         app.logger.error("Could not read 'Blog Name' key in charlotte.config file")
         return "A Charlotte Blog"
@@ -15,15 +16,17 @@ def get_blog_name():
 def get_charlotte_root():
     return pathlib.Path(__file__).parent.parent
 
-CONFIG_PATH = get_charlotte_root() / "charlotte.config"
+def get_config():
+    CONFIG_PATH = get_charlotte_root() / "charlotte.config"
+    try:
+        if not (CONFIG_PATH.exists() and CONFIG_PATH.is_file()):
+            default_config = get_charlotte_root() / "resources" / "charlotte.config"
+            shutil.copy(default_config, CONFIG_PATH)
 
-try:
-    if CONFIG_PATH.exists() and CONFIG_PATH.is_file():
-        config_file = ConfigParser()
-        config_file.read(CONFIG_PATH)
-    else:
-        default_config = get_charlotte_root() / "resources" / "charlotte.config"
-        shutil.copy(default_config, CONFIG_PATH)
-except Exception as exception:
-    app.logger.error("Could not load charlotte.config: {}".format(exception))
-    config_file = None
+        with open(CONFIG_PATH, "r") as config_file:
+            config = yaml.load(config_file)
+    except Exception as exception:
+        app.logger.error("Could not load charlotte.config: {}".format(exception))
+        config = None
+
+    return config
