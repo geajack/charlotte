27639f1526cdf4dbb7b2dcb6d0d9a56737b072ae
Jack <babybishops@gmail.com>
2019-02-04 10:39:59 +0100

    Catch errors better when initializing database

--- charlotte/articles.py
+++ charlotte/articles.py
@@ -59,26 +59,24 @@ class Article:
 
 def initialize():
     try:
-        connection = sqlite3.connect("articles/database.db")
-        connection.execute(
-            """
-            CREATE TABLE IF NOT EXISTS articles
-                (
-                    id integer PRIMARY KEY,
-                    title text NOT NULL,
-                    author text,
-                    format text,
-                    date datetime,
-                    slug text UNIQUE
-                );
-            """
-        )
+        with sqlite3.connect("articles/database.db") as connection:
+            connection.execute(
+                """
+                CREATE TABLE IF NOT EXISTS articles
+                    (
+                        id integer PRIMARY KEY,
+                        title text NOT NULL,
+                        author text,
+                        format text,
+                        date datetime,
+                        slug text UNIQUE
+                    );
+                """
+            )
         charlotte_root = settings.get_charlotte_root()
         (charlotte_root / "articles" / "content").mkdir(exist_ok=True)
     except:
         app.logger.error("Could not initialize database")
-    finally:
-        connection.close()    
 
 def slug_from_title(title):
     return slugify.slugify(title)
