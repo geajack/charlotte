ac8d8153c8ff6dde1c70411b3a4ec0195fbf9348
Jack <babybishops@gmail.com>
2019-02-09 16:33:13 +0100

    Implement authentication as a decorator

--- charlotte/api.py
+++ charlotte/api.py
@@ -8,18 +8,26 @@ class CharlotteAPIException(Exception):
 class UnauthorizedException(CharlotteAPIException):
     pass
 
+def authenticate(operation):
+    def authenticated_operation(*args, **kwargs, password=None):
+        if settings.is_password_correct(password):
+            return operation(*args, **kwargs)
+        else:
+            raise UnauthorizedException()
+
+    return authenticated_operation
+
+@authenticate
 def get_articles():
     all_articles = articles.get_all()
     headers = [article.as_api_header() for article in all_articles]
     return headers
 
-def post_article(title, author, article_format, content, password):
-    authenticated = settings.is_password_correct(password)
-    if authenticated:
-        articles.post_article(title, author, article_format, content)
-    else:
-        raise UnauthorizedException()
+@authenticate
+def post_article(title, author, article_format, content):
+    articles.post_article(title, author, article_format, content)
 
+@authenticate
 def get_article(article_id):
     article = articles.get_article_by_id(article_id)
     if article is not None:
@@ -27,20 +35,15 @@ def get_article(article_id):
     else:
         raise CharlotteAPIException()
 
-def delete_article(article_id, password):    
-    authenticated = settings.is_password_correct(password)
-    if authenticated:
-        articles.delete_article(article_id)
-    else:
-        raise UnauthorizedException()
+@authenticate
+def delete_article(article_id, password):
+    articles.delete_article(article_id)
 
+@authenticate
 def update_article(article_id, title, author, article_format, content, password):
-    authenticated = settings.is_password_correct(password)
-    if authenticated:
-        articles.update_article(article_id, title=title, author=author, format=article_format, content=content)
-    else:
-        raise UnauthorizedException()
+    articles.update_article(article_id, title=title, author=author, format=article_format, content=content)
 
+@authenticate
 def get_formats():
     formats = settings.get_formats()
     return [format_object.as_api_entity() for format_object in formats]
