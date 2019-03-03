5742c28e46465ffbaa9ead3b6ea73be9c9a99660
Jack <babybishops@gmail.com>
2019-03-03 11:47:17 +0100

    Move template loader code

--- charlotte/__init__.py
+++ charlotte/__init__.py
@@ -1,5 +1,31 @@
 from flask import Flask
+from jinja2 import BaseLoader
+from jinja2.parser import Parser
+from jinja2.exceptions import TemplateNotFound
 
 app = Flask("Charlotte")
+
+class TemplateLoader(BaseLoader):
+
+    def __init__(self):
+        pass
+
+    def get_source(self, environment, name):
+        theme_folder = settings.get_theme_directory()
+        if theme_folder is None:
+            raise TemplateNotFound(name)
+
+        template_path = theme_folder / "templates" / name
+        
+        try:
+            with open(template_path) as f:
+                return f.read(), str(template_path), self.use_cache.__get__(self, TemplateLoader)
+        except FileNotFoundError:
+            raise TemplateNotFound(name)
+
+    def use_cache(self, path):
+        return False
+
+app.jinja_loader = TemplateLoader()
 
 import charlotte.routes
--- charlotte/routes/view.py
+++ charlotte/routes/view.py
@@ -2,36 +2,10 @@ import codecs
 
 from flask import render_template, Markup
 import flask
-from jinja2 import BaseLoader
-from jinja2.parser import Parser
-from jinja2.exceptions import TemplateNotFound
 
 from charlotte import app
 from charlotte import articles
 from charlotte import settings
-
-class CharlotteTemplateLoader(BaseLoader):
-
-    def __init__(self):
-        pass
-
-    def get_source(self, environment, name):
-        theme_folder = settings.get_theme_directory()
-        if theme_folder is None:
-            raise TemplateNotFound(name)
-
-        template_path = theme_folder / "templates" / name
-        
-        try:
-            with open(template_path) as f:
-                return f.read(), str(template_path), self.use_cache.__get__(self, CharlotteTemplateLoader)
-        except FileNotFoundError:
-            raise TemplateNotFound(name)
-
-    def use_cache(self, path):
-        return False
-
-app.jinja_loader = CharlotteTemplateLoader()
 
 @app.context_processor
 def inject():
