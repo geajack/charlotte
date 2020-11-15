08ae1ac7d8f4c0bd0caf6388ea67c448cec837d9
Jack <->
2020-11-15 23:40:30 +0000

    Improve init command

--- charlotte/cli.py
+++ charlotte/cli.py
@@ -2,26 +2,38 @@ from pathlib import Path
 from shutil import copytree, copy
 from sys import argv
 
+from jinja2 import Template
+
 import charlotte.articles
 
-def init(path):
-    target = Path(path)
-    resources_directory = Path(__file__).parent / "resources"
-    copytree(
-        src=resources_directory / "renderers",
-        dst=target / "renderers"
-    )
-    copytree(
-        src=resources_directory / "themes",
-        dst=target / "themes"
-    )
-    copy(
-        src=resources_directory / "charlotte.config",
-        dst=target
-    )
+def init(path, name, password):
+    try:
+        target = Path(path)
+        resources_directory = Path(__file__).parent / "resources"
+        copytree(
+            src=resources_directory / "renderers",
+            dst=target / "renderers"
+        )
+        copytree(
+            src=resources_directory / "themes",
+            dst=target / "themes"
+        )
+        
+        with open(resources_directory / "charlotte.config.jinja") as file:
+            template = Template(file.read())
+        
+        with open(target / "charlotte.config", "w") as file:
+            file.write(template.render(name=name, password=password))
 
-    charlotte.articles.initialize(path)
+        charlotte.articles.initialize(path)
+    except FileExistsError:
+        print("Please erase the contents of this directory and try again.")
 
 def main():
     if argv[1] == "init":
-        init(".")
+        name = input("Blog name: ")
+        password = input("Password: ")
+        init(".", name, password)
+
+if __name__ == "__main__":
+    main()
